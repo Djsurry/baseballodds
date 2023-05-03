4ad7dcb84d55f64aa9b44921d8805b1bd36302af
David Surry <djsurry@gmail.com>
2023-05-03 19:56:17 -0400

    made games not stupid

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import os, requests, json
 API_KEY = os.environ['ODDS_KEY']
 
 def get_games():
-  return [x['id'] for x in json.loads(requests.get(f'https://api.the-odds-api.com/v4/sports/baseball_mlb/odds/?apiKey={API_KEY}&regions=us&markers=h2h&oddsFormat=decimal').text)]
+  return [{'home_team': x['home_team'], 'away_team': x['away_team'], 'id': x['id']} for x in json.loads(requests.get(f'https://api.the-odds-api.com/v4/sports/baseball_mlb/odds/?apiKey={API_KEY}&regions=us&markers=h2h&oddsFormat=decimal').text)]
 
 def get_odds(event, markets):
   return [x for x in json.loads(requests.get(f'https://api.the-odds-api.com/v4/sports/baseball_mlb/events/{event}/odds?apiKey={API_KEY}&regions=us&markets={markets}&oddsFormat=decimal').text)['bookmakers'] if x['key'] == 'fanduel'][0]['markets']
 
 # batter_home_runs
 # batter_hits	
-print(get_odds(get_games()[0], "batter_home_runs"))
+print(get_odds(get_games()[0]['id'], "batter_home_runs"))
